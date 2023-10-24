9abef1c8c534d4598ef36b3b825b610efe05d176
ecouseray <ecouseray@gmail.com>
2023-10-24 21:05:42 +0800

    test_2023_10_24

---
+++ test_2023_10_24/test_2023_10_24/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//struct ListNode* reverseList(struct ListNode* head) {
+//
+//	struct ListNode* newHead = NULL;
+//	struct ListNode* cur = head;
+//
+//
+//	while (cur != NULL)
+//	{
+//		struct ListNode* next = cur->next;
+//		cur->next = newHead;
+//		newHead = cur;
+//		cur = next;
+//	}
+//	return newHead;
+//}
+
+// 思路一：三个指针反转
+// 思路二：头插法
+
+
+struct ListNode* removeElements(struct ListNode* head, int val) {
+
+}
+// 移除链表元素：
+// 
+// 
+
+int main()
+{
+
+	return 0;
+}
+
