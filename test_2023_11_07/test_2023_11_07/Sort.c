bba32f313f903e01386bec5622040b02f8e0ff08
ecouseray <ecouseray@gmail.com>
2023-11-07 20:52:34 +0800

    test_2023_11_07

---
+++ test_2023_11_07/test_2023_11_07/Sort.c
@@ -0,0 +1,93 @@
+#include "Sort.h"
+
+// 插入排序
+void InsertSort(int* a, int n)
+{
+	assert(a);
+
+	for (int i = 0; i < n - 1; ++i)
+	{
+		// end 是一个数组的下标
+		// 排序的思路是将end + 1的元素插入到[0, end]中
+		int end = i;
+		int tmp = a[end + 1];
+		while (end >= 0)
+		{
+			if (tmp < a[end])
+			{
+				a[end + 1] = a[end];
+				--end;
+			}
+			else
+			{
+				break;
+			}
+		}
+		a[end + 1] = tmp;
+	}
+}
+
+// 希尔排序
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;// +1 在这里的目的是保证最后一次gap一定是3
+		for (int i = 0; i < n - gap; ++i)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+	}
+	
+}
+
+static void Swap(int* p1, int* p2)
+{
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+void SelectSort(int* a, int n)
+{
+	int begin = 0, end = n - 1;
+	while (begin < end)
+	{
+		int maxi, mini;
+		maxi = mini = begin;
+		for (int i = begin; i <= end; ++i)
+		{
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
+		}
+		Swap(&a[begin], &a[mini]);
+		if (begin == maxi)
+		{
+			maxi = mini;
+		}
+		Swap(&a[end], &a[maxi]);
+		++begin, --end;
+	}
+	
+}
