c64832fb3b8ea5a8cada64fd1f273dd34d9aa024
ecouseray <ecouseray@gmail.com>
2023-11-11 16:43:59 +0800

    test_2023_11_11

--- test_2023_11_08/test_2023_11_08/Sort.c
+++ test_2023_11_08/test_2023_11_08/Sort.c
@@ -115,16 +115,6 @@ void BubbleSort(int* a, int n)
 		}
 		--end;
 	}
-	//for (int i = 0; i < n - 1; ++i)
-	//{
-	//	for (int j = 0; j < n - 1 - i; ++j)
-	//	{
-	//		if (a[j] > a[j + 1])
-	//		{
-	//			Swap(&a[j], &a[j + 1]);
-	//		}
-	//	}
-	//}
 }
 
 void ADjustDown(int* a, int n, int root)
--- test_2023_11_11/test_2023_11_11/main.c
+++ test_2023_11_11/test_2023_11_11/main.c
@@ -1,6 +1,6 @@
-#include "Sort.h"
+#include <stdio.h>
+#include <assert.h>
 
-// 快速排序
 
 int GetMidIndex(int* a, int begin, int end)
 {
@@ -71,31 +71,47 @@ int PartSort(int* arr, int begin, int end)
 }
 
 
-int PartSort(int* arr, int begin, int end)
+int PartSort2(int* arr, int begin, int end)
 {
 	int midIndex = GetMidIndex(arr, begin, end);
 	Swap(&arr[midIndex], &arr[end]);
 
-
-	int keyIndex = end;
+	int key = arr[end];
 	while (begin < end)
 	{
 		// begin 找大
-		while (begin < end && arr[begin] <= arr[keyIndex])
-		{
+		while (begin < end && arr[begin] <= arr[key])
 			++begin;
-		}
+		arr[end] = arr[begin];
 		// end 找小
-		while (begin < end && arr[end] >= arr[keyIndex])
-		{
+		while (begin < end && arr[end] >= arr[key])
 			--end;
-		}
-		Swap(&arr[begin], &arr[end]);
+		arr[begin] = arr[end];
 	}
-	Swap(&arr[begin], &arr[keyIndex]);
+	arr[begin] == key;
 	return begin;
 }
 
+
+int PartSort3(int* arr, int begin, int end)
+{
+	int mid = GetMidIndex(arr, begin, end);
+	Swap(&arr[mid], &arr[end]);
+	
+	int keyIndex = end;
+	int cur = begin;
+	int prev = begin - 1;
+	while (cur < end)
+	{
+		if (arr[cur] < arr[keyIndex] && ++prev != cur)
+			Swap(&arr[cur], &arr[prev]);
+
+		++cur;
+
+	}
+	Swap(&arr[keyIndex], &arr[++prev]);
+	return prev;
+}
 
 
 
@@ -103,13 +119,28 @@ int PartSort(int* arr, int begin, int end)
 void QuickSort(int* arr, int left, int right)
 {
 	assert(arr);
-	//if (left >= right)
-	//	return;
+	if (left >= right)
+		return;
 
-	if (left < right)
+	if (left - right + 1 > 10)
 	{
-		int div = PartSort(arr, left, right);
+
+		int div = PartSort3(arr, left, right);
 		QuickSort(arr, left, div - 1);
 		QuickSort(arr, div + 1, right);
 	}
+	else
+	{
+		InsertSort(arr + left, right - left + 1);
+	}
+
+}
+
+
+
+int main()
+{
+	int arr[] = { 6,1,2,7,9,3,4,8,10,5 };
+	QuickSort(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
+	return 0;
 }
